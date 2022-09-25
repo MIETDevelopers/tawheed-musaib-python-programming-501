e5826539bba708ca73a81e870b1f6a1c658e24ca
tawheedmusaib <81609140+tawheedmusaib@users.noreply.github.com>
2022-09-25 21:18:45 +0530

    Add files via upload

---
+++ activity3_19sept2022/StringsOperations.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Notebook Created By : Tawheed Musaib
+
+# Strings:
+
+# Python has a built-in string class name "str" with many handy features. String literals can be enclosed by either double or single quotes, although single quotes are more commonly used. Python strings are "immutable" which means they cannot be changed after they are created.
+
+# In[1]:
+
+
+# Creating String
+str1 = 'Hello World'
+print(str1)
+
+
+# In[2]:
+
+
+# Accessing string elements
+str2 = 'Learning Python'
+str2[2]
+
+
+# In[4]:
+
+
+# Slicing
+str3 = "My Name is Tawheed"
+str3[11:]
+
+
+# In[5]:
+
+
+# Reverse string
+str1 = 'Hello'
+str1[::-1]
+
+
+# In[6]:
+
+
+# String Concatenation
+str1 = 'I am Learning '
+str2 = 'Python'
+text = str1 + str2
+print(text)
+
+
+# str(): It converts values to a string form so they can be combines with other strings
+
+# In[7]:
+
+
+num = 123
+str1 = '456'
+text = str(num) + str1
+print(text)
+
+
+# In[ ]:
+
+
+
+
