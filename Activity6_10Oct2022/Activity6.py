ae049f6f15ed6e43c93287a75afb1e2909697e01
tawheedmusaib <81609140+tawheedmusaib@users.noreply.github.com>
2022-10-12 12:50:09 +0530

    Add files via upload

---
+++ Activity6_10Oct2022/Activity6.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Program to create a list of student's records and search a student record using a dictionary.
+#  Creating Dictionary
+students = dict()
+n = int(input("Enter the number of students: "))
+for i in range(n):
+    sname = input("Enter the name of a student: ")
+    marks = []
+    s = 3
+    for j in range(s):
+        m = float(input("Enter marks: "))
+        marks.append(m)
+        students[sname] = marks
+print("Student Dictionary Created: ")
+print(students)
+
+# Searching Dictionary
+print("Search Records:")
+name = input("Enter the name of Student: ")
+if name in students.keys():
+    print(students[name])
+else:
+    print("No Records found!")
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+
